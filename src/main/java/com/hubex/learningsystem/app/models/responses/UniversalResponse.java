5c4885045a9c8c623c40a8af8065eac5bb85a58b
hubert123490 <hubert1234.94@gmail.com>
2021-11-24 02:18:09 +0100

    added extra field to UniversalResponse

--- src/main/java/com/hubex/learningsystem/app/models/responses/UniversalResponse.java
+++ src/main/java/com/hubex/learningsystem/app/models/responses/UniversalResponse.java
@@ -1,13 +1,16 @@
 package com.hubex.learningsystem.app.models.responses;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class UniversalResponse {
     private String message;
     private String status;
+    private Long id = null;
 
     public UniversalResponse(String message, String status) {
         this.message = message;
