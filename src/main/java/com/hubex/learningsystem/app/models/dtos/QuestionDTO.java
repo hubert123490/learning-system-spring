45d3a73a44d6d6c39e9ae22b5d4428f7a2f3e70b
hubert123490 <hubert1234.94@gmail.com>
2021-11-29 01:22:49 +0100

    model for Question

---
+++ src/main/java/com/hubex/learningsystem/app/models/dtos/QuestionDTO.java
@@ -0,0 +1,23 @@
+package com.hubex.learningsystem.app.models.dtos;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class QuestionDTO implements Serializable {
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    private static final long serialVersionUID = 1552737363605250535L;
+
+    private Long id;
+    private String description;
+    private String type;
+    private List<QueryDTO> queries = new ArrayList<>();
+
+}
---
+++ src/main/java/com/hubex/learningsystem/app/models/entities/QuestionEntity.java
@@ -0,0 +1,44 @@
+package com.hubex.learningsystem.app.models.entities;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "questions")
+public class QuestionEntity implements Serializable {
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    private static final long serialVersionUID = -147816712845341470L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    private String correctAnswer;
+
+    private String type;
+
+    private int maxPoints;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+    Set<QueryEntity> queries = new HashSet<>();
+
+    @ManyToOne
+    @JoinColumn(name="exam_id")
+    private ExamEntity exam;
+
+    @OneToMany(mappedBy = "question")
+    Set<AnswerEntity> answers;
+}
